22c40fd5ad7d770e87bb33d169de4b7eaf2836bc
Martin Eden <m3den0@gmail.com>
2022-10-16 14:46:29 +0300

    add gnome-terminal and nautilus keybindings

--- scripts/apply_gnome_settings.sh
+++ scripts/apply_gnome_settings.sh
@@ -16,7 +16,7 @@ gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-6 "[]"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-7 "[]"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-8 "[]"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-9 "[]"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-0 "[]"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-10 "[]"
 
 # Disable switching between workspaces by super+1...
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "[]"
@@ -29,3 +29,19 @@ gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-6 "[]"
 # Switch windows instead of applications by Alt+Tab
 gsettings set org.gnome.desktop.wm.keybindings switch-applications '[]'
 # TODO
+
+###############################################################################
+# CUSTOM KEYS
+###############################################################################
+
+# Run gnome terminal by Ctrl+Alt+t
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'Terminal'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command 'gnome-terminal'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Control><Alt>t'
+
+# Run nautilus by Super+e
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/']"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'Nautilus'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command 'nautilus'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Super>e'
